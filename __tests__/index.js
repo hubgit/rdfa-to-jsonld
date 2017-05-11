9b73639eb3739599388e6173e6dacaccb9332473
Alf Eaton <eaton.alf@gmail.com>
2017-05-11 22:43:14 +0100

    Initial commit

---
+++ __tests__/index.js
@@ -0,0 +1,25 @@
+const extractor = require('../lib')
+
+test('module exports a function', () => {
+  expect(typeof extractor).toBe('function')
+})
+
+test('returns the expected value', async (done) => {
+  const url = 'http://www.bbc.co.uk/bbcone/programmes/schedules/london/2017/01/01'
+
+  const frame = {
+    '@context': {
+      '@vocab': 'http://schema.org/'
+    },
+    '@type': 'Episode'
+  }
+
+  try {
+    const result = await extractor(url, {frame, expand: true})
+    expect(result).toHaveLength(24)
+    expect(result[0]['@id']).toBe('http://www.bbc.co.uk/programmes/b0396mkd')
+    done()
+  } catch (e) {
+    done.fail(e)
+  }
+})
